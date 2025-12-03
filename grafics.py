e6d5a20afc306d5c835e5225fb4d78261f675f88
illiabashtovyicore <illia.bashtovyi@gmail.com>
2025-12-03 20:00:06 +0200

    add creation preview for new objects in graphics

--- grafics.py
+++ grafics.py
@@ -13,12 +13,63 @@ def start_graphics():
     screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
     font = pg.font.SysFont(None, 24)
 
-def draw_objects(objects, mult=None):
+def draw_objects(objects, mult=None, creating=None):
     global screen, font
     screen.fill((0, 0, 0))  # Clear screen with black
 
-    for obj in objects:
+    # draw objects with larger ones first so smaller objects appear on top
+    for obj in sorted(objects, key=lambda o: o.size, reverse=True):
         pg.draw.circle(screen, obj.color, (int(obj.x), int(obj.y)), int(obj.size))
+
+    # draw creation preview (translucent circle and velocity vector)
+    if creating:
+        # creating is expected to be a dict with keys: stage, x, y, rad, mouse_pos
+        stage = creating.get('stage', 0)
+        cx = int(creating.get('x', 0))
+        cy = int(creating.get('y', 0))
+        rad = creating.get('rad', 0)
+        mouse_pos = creating.get('mouse_pos', (0,0))
+
+        # translucent surface for preview circle
+        if stage >= 1 and rad > 0:
+            preview_surf = pg.Surface((rad*2+4, rad*2+4), pg.SRCALPHA)
+            preview_color = (200, 200, 255, 80)  # translucent bluish
+            pg.draw.circle(preview_surf, preview_color, (int(rad)+2, int(rad)+2), int(rad))
+            screen.blit(preview_surf, (cx - rad - 2, cy - rad - 2))
+
+            # outline
+            pg.draw.circle(screen, (200,200,255), (cx, cy), int(rad), 1)
+
+        # velocity vector preview (arrow)
+        if stage == 2:
+            mx, my = mouse_pos
+            # draw line from creation center to mouse
+            pg.draw.line(screen, (255,255,255), (cx, cy), (mx, my), 2)
+            # simple arrowhead
+            dx = mx - cx
+            dy = my - cy
+            length = max(1, (dx*dx + dy*dy) ** 0.5)
+            ux, uy = dx/length, dy/length
+            # two small lines for arrowhead
+            left = (mx - int(10*(ux - uy)), my - int(10*(uy + ux)))
+            right = (mx - int(10*(ux + uy)), my - int(10*(uy - ux)))
+            pg.draw.line(screen, (255,255,255), (mx, my), left, 2)
+            pg.draw.line(screen, (255,255,255), (mx, my), right, 2)
+
+        # small instruction text
+        if font is None:
+            font = pg.font.SysFont(None, 24)
+        if stage == 0:
+            instruct = "Left-click to set position"
+        elif stage == 1:
+            instruct = "Right-click to set radius (move mouse to preview)"
+        elif stage == 2:
+            instruct = "Left-click to set velocity (drag) and create object"
+        else:
+            instruct = ""
+        if instruct:
+            txt = font.render(instruct, True, (200,200,200))
+            screen.blit(txt, (10, 40))
 
     # draw time multiplier once per frame
     if font is None:
--- main.py
+++ main.py
@@ -13,9 +13,21 @@ mult = 1  # time multiplier
 count = 0
 x, y, vx, vy, rad = 0, 0, 0, 0, 0  # parameters for new object
 lst = [ph.OBJECT(400, 300, 10, 0, 0, 50000)]  # list of objects
+
+# creation preview dict
+creating = {'stage': 0, 'x': 0, 'y': 0, 'rad': 0, 'mouse_pos': (0, 0)}
+
 # main loop
 runing = True
 while runing:
+    # update live mouse position for preview
+    mouse_pos = pg.mouse.get_pos()
+    creating['mouse_pos'] = mouse_pos
+    creating['stage'] = count
+    creating['x'] = x
+    creating['y'] = y
+    creating['rad'] = rad
+
     # check for events
     for event in pg.event.get():
         # stop if escape is pressed
@@ -23,24 +35,35 @@ while runing:
             runing = False
         elif event.type == pg.QUIT:
             runing = False
-        # set position of new object
+
+        # set position of new object (first left click)
         if count == 0:
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 x, y = pg.mouse.get_pos()
-                count += 1
-        # set radius of new object
-        if count == 1:
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
-                print(1)
+                rad = 0
+                vx = vy = 0
+                count = 1
+                creating['x'], creating['y'] = x, y
+        # set radius of new object (right click while previewing radius)
+        elif count == 1:
+            # update preview radius continuously
+            if event.type == pg.MOUSEMOTION:
                 rad = ph.dist_points(x, y, *pg.mouse.get_pos())
-                count += 1
-        # set velocity of new object and create it
-        if count == 2:
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
+                rad = ph.dist_points(x, y, *pg.mouse.get_pos())
+                count = 2
+        # set velocity of new object and create it (left click)
+        elif count == 2:
+            # update preview velocity vector continuously
+            if event.type == pg.MOUSEMOTION:
+                vx, vy = (pg.mouse.get_pos()[0] - x)/10, (pg.mouse.get_pos()[1] - y)/10
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 vx, vy = (pg.mouse.get_pos()[0] - x)/10, (pg.mouse.get_pos()[1] - y)/10
                 lst.append(ph.OBJECT(x, y, rad, vx, vy))
+                # reset creation state
                 count = 0
-
+                x = y = vx = vy = rad = 0
+                creating = {'stage': 0, 'x': 0, 'y': 0, 'rad': 0, 'mouse_pos': (0, 0)}
 
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_UP:
@@ -48,7 +71,15 @@ while runing:
             if event.key == pg.K_DOWN:
                 mult -=1
 
-# update physics
+    # if user is in radius preview (mouse moved without event), update rad
+    if count == 1:
+        rad = ph.dist_points(x, y, *mouse_pos)
+        creating['rad'] = rad
+    if count == 2:
+        # update creating mouse_pos already set above; preview velocity will be drawn by grafics
+        pass
+
+    # update physics
     # calculate acceleration
     for obj in lst:
         for other in lst:
@@ -58,28 +89,9 @@ while runing:
     for obj in lst:
         obj.move()
 
-# update graphics
-    g.draw_objects(lst, mult)
+    # update graphics (pass creation preview)
+    g.draw_objects(lst, mult, creating)
 
-
-    # cap the frame rate
-    clock.tick(mult*ph.time) # time FPS
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # cap the frame rate (avoid passing 0)
+    target_fps = max(1, mult * ph.time)
+    clock.tick(target_fps)
